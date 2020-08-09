ff1ff30104802230e27ca0abf978c86ffab073e5
anthony cobb <anthonycobb@anthonys-MacBook-Pro-2.local>
2020-08-09 17:31:14 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// var numbers = [3, 56, 2, 48, 5];
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// //Map -Create a new array by doing something with each item in an array.
+// const addOne = numbers.map(function (x) {
+//   return x + 1;
+// });
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// console.log(addOne);
+
+// //Filter - Create a new array by keeping the items that return true.
+// const greaterThan10 = numbers.filter(function (x) {
+//   return x > 10;
+// });
+
+// console.log(greaterThan10);
+
+// //Reduce - Accumulate a value by doing something to each item in an array.
+
+// const foo = numbers.reduce(function (acc, curr) {
+//   console.log(`accumulator: ${acc}`);
+//   console.log(`current number: ${curr}`);
+//   return acc + curr;
+// });
+
+// console.log(foo);
+
+// //Find - find the first item that matches from an array.
+// const find = numbers.find(function (num) {
+//   return num > 10;
+// });
+
+// console.log(find);
+
+// //FindIndex - find the index of the first item that matches.
+// const findIndex = numbers.findIndex(function (num) {
+//   return num > 10;
+// });
+
+// console.log(findIndex);
+
+import emojipedia from "./emojipedia";
+
+//create an array that has the meanings from emojipedia, and they are truncated to 100 characters
+
+const meanings = emojipedia.map(function (element) {
+  return element.meaning;
+});
+
+console.log(meanings);
